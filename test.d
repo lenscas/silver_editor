abf587b8a6ecb309b6d2346e306c215cf3c97ed1
lenscas <lenscas@users.noreply.github.com>
2020-07-12 20:02:52 +0000

    deploy: d8ff989a03b0d560bdceb6db5a8d5cbb5428e17f

---
+++ test.d
@@ -0,0 +1,5 @@
+/home/runner/work/silver_editor/silver_editor/target/wasm32-unknown-unknown/release/examples/test.wasm: examples/test.rs
+
+/home/runner/work/silver_editor/silver_editor/target/wasm32-unknown-unknown/release/examples/test.d: examples/test.rs
+
+examples/test.rs:
